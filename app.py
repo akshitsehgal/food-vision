9d601431e5404334d9bb2bc493a06bfcc68f3982
Akshit Sehgal <84592564+akshitsehgal@users.noreply.github.com>
2021-08-12 13:53:47 +0530

    food vision project

---
+++ app.py
@@ -0,0 +1,134 @@
+import tensorflow as tf
+from flask import Flask, render_template, request
+from keras.models import load_model
+from keras.preprocessing import image
+
+
+app =  Flask(__name__)
+
+class_names = ['apple_pie','baby_back_ribs','baklava',
+ 'beef_carpaccio',
+ 'beef_tartare',
+ 'beet_salad',
+ 'beignets',
+ 'bibimbap',
+ 'bread_pudding',
+ 'breakfast_burrito',
+ 'bruschetta',
+ 'caesar_salad',
+ 'cannoli',
+ 'caprese_salad',
+ 'carrot_cake',
+ 'ceviche',
+ 'cheesecake',
+ 'cheese_plate',
+ 'chicken_curry',
+ 'chicken_quesadilla',
+ 'chicken_wings',
+ 'chocolate_cake',
+ 'chocolate_mousse',
+ 'churros',
+ 'clam_chowder',
+ 'club_sandwich',
+ 'crab_cakes',
+ 'creme_brulee',
+ 'croque_madame',
+ 'cup_cakes',
+ 'deviled_eggs',
+ 'donuts',
+ 'dumplings',
+ 'edamame',
+ 'eggs_benedict',
+ 'escargots',
+ 'falafel',
+ 'filet_mignon',
+ 'fish_and_chips',
+ 'foie_gras',
+ 'french_fries',
+ 'french_onion_soup',
+ 'french_toast',
+ 'fried_calamari',
+ 'fried_rice',
+ 'frozen_yogurt',
+ 'garlic_bread',
+ 'gnocchi',
+ 'greek_salad',
+ 'grilled_cheese_sandwich',
+ 'grilled_salmon',
+ 'guacamole',
+ 'gyoza',
+ 'hamburger',
+ 'hot_and_sour_soup',
+ 'hot_dog',
+ 'huevos_rancheros',
+ 'hummus',
+ 'ice_cream',
+ 'lasagna',
+ 'lobster_bisque',
+ 'lobster_roll_sandwich',
+ 'macaroni_and_cheese',
+ 'macarons',
+ 'miso_soup',
+ 'mussels',
+ 'nachos',
+ 'omelette',
+ 'onion_rings',
+ 'oysters',
+ 'pad_thai',
+ 'paella',
+ 'pancakes',
+ 'panna_cotta',
+ 'peking_duck',
+ 'pho',
+ 'pizza',
+ 'pork_chop',
+ 'poutine',
+ 'prime_rib',
+ 'pulled_pork_sandwich',
+ 'ramen',
+ 'ravioli',
+ 'red_velvet_cake',
+ 'risotto',
+ 'samosa',
+ 'sashimi',
+ 'scallops',
+ 'seaweed_salad',
+ 'shrimp_and_grits',
+ 'spaghetti_bolognese',
+ 'spaghetti_carbonara',
+ 'spring_rolls',
+ 'steak',
+ 'strawberry_shortcake',
+ 'sushi',
+ 'tacos',
+ 'takoyaki',
+ 'tiramisu',
+ 'tuna_tartare',
+ 'waffles']
+
+
+model = load_model('101_food_classes_fine_tuned2.h5')
+
+
+@app.route('/', methods=['GET','POST'])
+def main():
+  return render_template('index.html')
+
+@app.route('/submit', methods=['GET','POST'])
+def predict():
+  imagefile = request.files['imagefile']
+  img_path = './images/' + imagefile.filename
+  imagefile.save(img_path)
+  image = tf.keras.preprocessing.image.load_img(img_path,target_size=(224,224)) 
+  image = tf.keras.preprocessing.image.img_to_array(image)
+  pred_prob= model.predict(tf.expand_dims(image,axis=0))
+  pred_class = class_names[pred_prob.argmax()]
+  classification = pred_class
+  
+  return render_template('index.html',prediction = classification, img_path = img_path)
+
+
+if __name__ =='__main__':
+  app.run(debug=False)
+
+  
